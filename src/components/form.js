381ee182844ebfdb4ba10da48dc7539a9c703cc9
iv150320 <johnrodin1@gmail.com>
2020-11-07 18:37:43 +0300

    Первый коммит в погоде

---
+++ src/components/form.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Form = (props)  => (
+
+<form onSubmit={props.weatherMethod}>
+
+<input type="text" name="city" placeholder="Введите название города"/>
+
+<button>Получить погоду</button>
+
+</form>
+
+);
+
+
+export default Form;
---
+++ src/components/info.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Info = () => (
+
+<div>
+
+<h2>Погода <br/> от Ивана Родина</h2>
+
+<p>Узнайте погоду в вашем городе</p>
+
+</div>
+
+);
+
+
+export default Info;
