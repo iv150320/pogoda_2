68db8adbc5f561a9a57d186f7261712ffedf1100
iv150320 <johnrodin1@gmail.com>
2021-01-28 10:41:07 +0300

    Updates

---
+++ precache-manifest.3a3a6bdfa553e76014a746781e750332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a5e7aedf4b654edc479ba7a2bee645",
+    "url": "/react-app_280121/index.html"
+  },
+  {
+    "revision": "9f5507d5ae63d53bcbd5",
+    "url": "/react-app_280121/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "f2c97440c4932dac087b",
+    "url": "/react-app_280121/static/css/main.5b6e81ca.chunk.css"
+  },
+  {
+    "revision": "9f5507d5ae63d53bcbd5",
+    "url": "/react-app_280121/static/js/2.18493a4d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-app_280121/static/js/2.18493a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c97440c4932dac087b",
+    "url": "/react-app_280121/static/js/main.6a237ad2.chunk.js"
+  },
+  {
+    "revision": "22191767738367e1d1d4",
+    "url": "/react-app_280121/static/js/runtime-main.3ee36b4b.js"
+  }
+]);
